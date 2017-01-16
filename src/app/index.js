6e5b39c85e7cfc23ef960878fdd90e49263afa33
Luis Herranz <luisherranz@gmail.com>
2017-01-16 18:34:32 +0100

    Export actions, types and creators.

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,17 @@
-import * as sagas from './sagas';
+import * as actions from './actions';
+import * as types from './types';
 import * as reducers from './reducers';
+import * as reducerCreators from './reducerCreators';
+import * as sagas from './sagas';
+import * as sagaCreators from './sagaCreators';
+import * as deps from './deps';
 
 export {
   sagas,
+  sagaCreators,
   reducers,
+  reducerCreators,
+  actions,
+  types,
+  deps,
 };
